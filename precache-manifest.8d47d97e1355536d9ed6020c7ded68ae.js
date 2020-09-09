bf4ee4dba2611de6de77f083b2a5e70a169b7c14
Stacey R. Brodsky <monkeymynd@gmail.com>
2020-09-09 13:33:54 -0500

    Updates

---
+++ precache-manifest.8d47d97e1355536d9ed6020c7ded68ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba034fba78feb2f5dc5ad24c0b07454c",
+    "url": "/vinfo/index.html"
+  },
+  {
+    "revision": "edc1376bcb6ac67feeed",
+    "url": "/vinfo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "58689110a1418ab0568d",
+    "url": "/vinfo/static/css/main.75504801.chunk.css"
+  },
+  {
+    "revision": "edc1376bcb6ac67feeed",
+    "url": "/vinfo/static/js/2.3fb4fd01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vinfo/static/js/2.3fb4fd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58689110a1418ab0568d",
+    "url": "/vinfo/static/js/main.b6132290.chunk.js"
+  },
+  {
+    "revision": "7e9f6600db7b00f8d4b8",
+    "url": "/vinfo/static/js/runtime-main.df68b731.js"
+  },
+  {
+    "revision": "927c51a3f5b75b420846c80a6679f4af",
+    "url": "/vinfo/static/media/car-vector.927c51a3.png"
+  },
+  {
+    "revision": "c397ba08a4d48f0d431a63b15f5c15fb",
+    "url": "/vinfo/static/media/gears.c397ba08.jpg"
+  }
+]);
